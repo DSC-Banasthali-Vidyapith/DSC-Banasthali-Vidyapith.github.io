814d294ff37c72cee845de2b9a28e2a6234e0f9f
Vanisha Vaish <vanisha.vaish@gmail.com>
2021-03-05 23:17:26 +0530

    Updated events page

--- events/index2.js
+++ events/index2.js
@@ -4,84 +4,76 @@
 const dat = [
 
     {
-    id:0,
-    pic: "../assets/images/final.jpeg",
-    title:"Skill It Up!",
-    venue:"Youtube",
-    link:"https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-skill-it-up/",
-    ylink:"https://www.youtube.com/channel/UCfDFc_J9x-yCE2Sr0eOJWuQ"
+        id: 2,
+        pic: "../assets/images/spring-boot.jpg",
+        title: "Building Java Project with Spring Boot",
+        venue: "Youtube",
+        link: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-building-java-project-with-spring-boot/",
+        ylink: "https://www.youtube.com/watch?v=R4KlVtNn7WM"
 
-  },
+    },
     {
-    id: 1,
-    pic: "images/Copy of DSC-Website-EventThumbnail.png",
-    time: "Thursday, 28 January 6:30 pm",
-    title: "DSC Solution Challenge 2021 Kickstart",
+        id: 1,
+        pic: "../assets/images/ML_img.jpg",
+        title: "Insights into Machine Learning",
+        link: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-insights-into-machine-learning-dsc-banasthali-vidyapith/",
+        ylink: "https://www.youtube.com/watch?v=mccO1R7jxL8"
+    },
+    {
+        id: 0,
+        pic: "../assets/images/node-js_img.png",
+        title: "Step into the World of NodeJS ",
+        link: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-step-into-the-world-of-nodejs-dsc-banasthali-vidyapith/",
+        ylink: "https://www.youtube.com/watch?v=N1b_mJufdAk"
 
-    link:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-solution-challenge-2021-kickstart-your-journey/#/",
-      ylink:"https://www.youtube.com/watch?v=bsEns6zcXHw&feature=emb_err_woyt"
-  },
-  {
-    id:2,
-    pic: "images/upcoming1.png",
-    time:"Tuesday, 26 January 3:00 pm",
-    title:"DSC DRUM ROLLS - Orientation Session",
-    
-    link:"https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-dsc-drum-rolls-orientation-session/#/",
-    ylink:"https://www.youtube.com/channel/UCfDFc_J9x-yCE2Sr0eOJWuQ"
+    }
 
-} 
-    
 ];
 
 
 const buildCard = event => {
     // Create elements needed to build a card
-const div = document.createElement('div');
-const h3 = document.createElement('h3');
-const a = document.createElement('a');
-const img = document.createElement('img');
-const p = document.createElement('a');
-const h4 = document.createElement('h4');
+    const div = document.createElement('div');
+    const h3 = document.createElement('h3');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    const p = document.createElement('a');
+    const h4 = document.createElement('h4');
 
 
 
-// Append newly created elements into the DOM
-const body = document.querySelector('.past-div');
-body.append(div);
+    // Append newly created elements into the DOM
+    const body = document.querySelector('.past-div');
+    body.append(div);
 
-div.append(img)
-div.append(h4)
-div.append(h3)
-div.append(p)
-div.append(a)
+    div.append(img)
+    div.append(h4)
+    div.append(h3)
+    div.append(p)
+    div.append(a)
 
 
 
-  // Set content and attributes
-div.setAttribute('class', 'card')
+    // Set content and attributes
+    div.setAttribute('class', 'card')
 
-img.setAttribute('src', event.pic)
+    img.setAttribute('src', event.pic)
 
-img.setAttribute('class', 'card-img-top')
+    img.setAttribute('class', 'card-img-top')
 
-h3.setAttribute('class','card-title');
-h3.innerHTML=event.title;
+    h3.setAttribute('class', 'card-title');
+    h3.innerHTML = event.title;
 
-p.setAttribute('class','card-text');
-p.setAttribute('href',event.ylink);
-p.innerHTML='<i class="fab fa-youtube"></i>';
+    p.setAttribute('class', 'card-text');
+    p.setAttribute('href', event.ylink);
+    p.innerHTML = '<i class="fab fa-youtube"></i>';
 
-a.setAttribute('class','event-link');
-a.innerHTML= "Read More";
-a.setAttribute('href',event.link);
+    a.setAttribute('class', 'event-link');
+    a.innerHTML = "Read More";
+    a.setAttribute('href', event.link);
 
-h4.innerHTML= "Completed";
-h4.setAttribute('class','badge')
+    h4.innerHTML = "Completed";
+    h4.setAttribute('class', 'badge')
 };
 
-  dat.forEach(event => buildCard(event));
-  
-
-
+dat.forEach(event => buildCard(event));
--- events/pagination.js
+++ events/pagination.js
@@ -1,166 +1,160 @@
-var tableData = [
-  {
-    event_name: "Skill It Up!",
-    date: "30-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-skill-it-up/",
-  },
-  {
-    event_name: "Solution Challenge 2021 - Kickstart Your Journey!",
-    date: "28-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-solution-challenge-2021-kickstart-your-journey/",
-  },
-  {
-    event_name: "DSC DRUM ROLLS-Orientation Session",
-    date: "26-01-2021",
-    venue: "Google Meet",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-dsc-drum-rolls-orientation-session/#/",
-  },
-  {
-    event_name: "Let's Open Source",
-    date: "23-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-lets-open-source/",
-  },
-  {
-    event_name: "Android Development",
-    date: "22-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-android-development/",
-  },
+var tableData = [{
+        event_name: "Building Java Project with Spring Boot",
+        date: "27-02-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-building-java-project-with-spring-boot/",
+    },
+    {
+        event_name: "Insights into Machine Learning",
+        date: "21-02-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-insights-into-machine-learning-dsc-banasthali-vidyapith/",
+    },
+    {
+        event_name: "Step into the World of NodeJS",
+        date: "20-02-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-step-into-the-world-of-nodejs-dsc-banasthali-vidyapith/",
+    },
+    {
+        event_name: "Skill It Up!",
+        date: "30-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-skill-it-up/",
+    },
+    {
+        event_name: "Solution Challenge 2021 - Kickstart Your Journey!",
+        date: "28-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-solution-challenge-2021-kickstart-your-journey/",
+    },
+    {
+        event_name: "DSC DRUM ROLLS-Orientation Session",
+        date: "26-01-2021",
+        venue: "Google Meet",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-dsc-drum-rolls-orientation-session/#/",
+    },
+    {
+        event_name: "Let's Open Source",
+        date: "23-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-lets-open-source/",
+    },
+    {
+        event_name: "Android Development",
+        date: "22-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-android-development/",
+    },
 
-  {
-    event_name: "LET'S APP IT",
-    date: "19-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-lets-app-it/",
-  },
-  {
-    event_name: "Let's Android",
-    date: "16-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-lets-android/",
-  },
-  {
-    event_name: "Android 101",
-    date: "12-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-android-101/",
-  },
-  {
-    event_name: "Arrays and Bit Manipulation",
-    date: "09-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-arrays-and-bit-manipulation/",
-  },
-  {
-    event_name: "Become an Android Developer",
-    date: "06-01-2021",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-become-an-android-developer/",
-  },
-  {
-    event_name: "CSS 101 of Web Development",
-    date: "30-12-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-css-101-of-web-development/",
-  },
-  {
-    event_name: "Web Development",
-    date: "27-12-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-web-development/",
-  },
-  {
-    event_name: "Android Study Jam",
-    date: "27-12-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-android-study-jam/",
-  },
-  {
-    event_name: "Fireside with flutter Expert",
-    date: "22-12-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-fireside-with-flutter-expert/",
-  },
-  {
-    event_name: "DSC WOW",
-    date: "10-12-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-dsc-wow-indias-biggest-collaboration/",
-  },
-  {
-    event_name: "Number Theory and Mathematics",
-    date: "5-12-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-number-theory-and-mathematics/",
-  },
-  {
-    event_name: "Learning From Experiences",
-    date: "19-11-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-learning-from-experiences/",
-  },
-  {
-    event_name: "Get it Done with the Experts",
-    date: "15-11-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-get-it-done-with-the-experts/",
-  },
-  {
-    event_name: "Deep Dive into Google Cloud",
-    date: "24-10-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-deep-dive-into-google-cloud/",
-  },
-  {
-    event_name: "Hacktoberfest Meetup",
-    date: "29-09-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-hacktoberfest-meetup/",
-  },
-  {
-    event_name: "Data Structures & Algorithms",
-    date: "27-09-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-data-structures-algorithms/",
-  },
-  {
-    event_name: "Introduction to Git/Github",
-    date: "20-09-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-introduction-to-gitgithub/",
-  },
-  {
-    event_name: "almas de ingeniería",
-    date: "15-09-2020",
-    venue: "Youtube",
-    see_more:
-      "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-almas-de-ingenieria",
-  },
+    {
+        event_name: "LET'S APP IT",
+        date: "19-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-lets-app-it/",
+    },
+    {
+        event_name: "Let's Android",
+        date: "16-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-lets-android/",
+    },
+    {
+        event_name: "Android 101",
+        date: "12-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-android-101/",
+    },
+    {
+        event_name: "Arrays and Bit Manipulation",
+        date: "09-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-arrays-and-bit-manipulation/",
+    },
+    {
+        event_name: "Become an Android Developer",
+        date: "06-01-2021",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-become-an-android-developer/",
+    },
+    {
+        event_name: "CSS 101 of Web Development",
+        date: "30-12-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-css-101-of-web-development/",
+    },
+    {
+        event_name: "Web Development",
+        date: "27-12-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-web-development/",
+    },
+    {
+        event_name: "Android Study Jam",
+        date: "27-12-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-android-study-jam/",
+    },
+    {
+        event_name: "Fireside with flutter Expert",
+        date: "22-12-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-fireside-with-flutter-expert/",
+    },
+    {
+        event_name: "DSC WOW",
+        date: "10-12-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-dsc-wow-indias-biggest-collaboration/",
+    },
+    {
+        event_name: "Number Theory and Mathematics",
+        date: "5-12-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-number-theory-and-mathematics/",
+    },
+    {
+        event_name: "Learning From Experiences",
+        date: "19-11-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-learning-from-experiences/",
+    },
+    {
+        event_name: "Get it Done with the Experts",
+        date: "15-11-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-get-it-done-with-the-experts/",
+    },
+    {
+        event_name: "Deep Dive into Google Cloud",
+        date: "24-10-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-deep-dive-into-google-cloud/",
+    },
+    {
+        event_name: "Hacktoberfest Meetup",
+        date: "29-09-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-hacktoberfest-meetup/",
+    },
+    {
+        event_name: "Data Structures & Algorithms",
+        date: "27-09-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-data-structures-algorithms/",
+    },
+    {
+        event_name: "Introduction to Git/Github",
+        date: "20-09-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-introduction-to-gitgithub/",
+    },
+    {
+        event_name: "almas de ingeniería",
+        date: "15-09-2020",
+        venue: "Youtube",
+        see_more: "https://dsc.community.dev/events/details/developer-student-clubs-banasthali-university-presents-almas-de-ingenieria",
+    },
 ];
 
 /*
@@ -169,91 +163,91 @@ var tableData = [
 */
 
 var state = {
-  querySet: tableData,
+    querySet: tableData,
 
-  page: 1,
-  rows: 3,
-  window: 3,
+    page: 1,
+    rows: 3,
+    window: 3,
 };
 
 buildTable();
 
 function pagination(querySet, page, rows) {
-  var trimStart = (page - 1) * rows;
-  var trimEnd = trimStart + rows;
+    var trimStart = (page - 1) * rows;
+    var trimEnd = trimStart + rows;
 
-  var trimmedData = querySet.slice(trimStart, trimEnd);
+    var trimmedData = querySet.slice(trimStart, trimEnd);
 
-  var pages = Math.round(querySet.length / rows);
+    var pages = Math.round(querySet.length / rows);
 
-  return {
-    querySet: trimmedData,
-    pages: pages,
-  };
+    return {
+        querySet: trimmedData,
+        pages: pages,
+    };
 }
 
 function pageButtons(pages) {
-  var wrapper = document.getElementById("pagination-wrapper");
+    var wrapper = document.getElementById("pagination-wrapper");
 
-  wrapper.innerHTML = ``;
-  // console.log("Pages:", pages);
+    wrapper.innerHTML = ``;
+    // console.log("Pages:", pages);
 
-  var maxLeft = state.page - Math.floor(state.window / 2);
-  var maxRight = state.page + Math.floor(state.window / 2);
-
-  if (maxLeft < 1) {
-    maxLeft = 1;
-    maxRight = state.window;
-  }
-
-  if (maxRight > pages) {
-    maxLeft = pages - (state.window - 1);
+    var maxLeft = state.page - Math.floor(state.window / 2);
+    var maxRight = state.page + Math.floor(state.window / 2);
 
     if (maxLeft < 1) {
-      maxLeft = 1;
+        maxLeft = 1;
+        maxRight = state.window;
     }
-    maxRight = pages;
-  }
 
-  for (var page = maxLeft; page <= maxRight; page++) {
-    wrapper.innerHTML += `<button value=${page} class="page btns btn-sm btn-info">${page}</button>`;
-  }
+    if (maxRight > pages) {
+        maxLeft = pages - (state.window - 1);
 
-  if (state.page != 1) {
-    wrapper.innerHTML =
-      `<button value=${1} class="page btns btn-sm btn-info">&#171; </button>` +
-      wrapper.innerHTML;
-  }
+        if (maxLeft < 1) {
+            maxLeft = 1;
+        }
+        maxRight = pages;
+    }
 
-  if (state.page != pages) {
-    wrapper.innerHTML += `<button value=${pages} class="page btns btn-sm btn-info"> &#187;</button>`;
-  }
+    for (var page = maxLeft; page <= maxRight; page++) {
+        wrapper.innerHTML += `<button value=${page} class="page btns btn-sm btn-info">${page}</button>`;
+    }
 
-  $(".page").on("click", function () {
-    $("#table-body").empty();
+    if (state.page != 1) {
+        wrapper.innerHTML =
+            `<button value=${1} class="page btns btn-sm btn-info">&#171; </button>` +
+            wrapper.innerHTML;
+    }
 
-    state.page = Number($(this).val());
+    if (state.page != pages) {
+        wrapper.innerHTML += `<button value=${pages} class="page btns btn-sm btn-info"> &#187;</button>`;
+    }
 
-    buildTable();
-  });
+    $(".page").on("click", function() {
+        $("#table-body").empty();
+
+        state.page = Number($(this).val());
+
+        buildTable();
+    });
 }
 
 function buildTable() {
-  var table = $("#table-body");
+    var table = $("#table-body");
 
-  var data = pagination(state.querySet, state.page, state.rows);
-  var myList = data.querySet;
+    var data = pagination(state.querySet, state.page, state.rows);
+    var myList = data.querySet;
 
-  for (var i = 1 in myList) {
-    //Keep in mind we are using "Template Litterals to create rows"
-    var row = `<tr>
+    for (var i = 1 in myList) {
+        //Keep in mind we are using "Template Litterals to create rows"
+        var row = `<tr>
                   <td>${myList[i].event_name}</td>
                   <td>${myList[i].date}</td>
                   <td>${myList[i].venue}</td>
                   <td><a href="${myList[i].see_more}">READ MORE</a></td>
                   `;
-    table.append(row);
-  }
+        table.append(row);
+    }
 
-  pageButtons(data.pages);
+    pageButtons(data.pages);
 }
